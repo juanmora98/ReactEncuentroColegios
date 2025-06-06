ea119a5f1aae2da4aba3e6e9ac575239e84cac3a
juan felipe mora <43444251+juanmora98@users.noreply.github.com>
2025-06-06 17:42:03 -0500

    HOTFIX

---
+++ decanaturas/src/pages/PagesDecanaturas/PagesIngEstadistica/components/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from 'core/components/NavBar/components/NavBar';
+
+function Index() {
+  return (
+    <div>
+      <NavBar ing="estadistica" ingName="ingenieria de estadistica" />
+    </div>
+  );
+}
+
+export default Index;
