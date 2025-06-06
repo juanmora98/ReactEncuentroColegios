da35b717e6bc26bbcc76642b2898926869349e86
juan felipe mora <43444251+juanmora98@users.noreply.github.com>
2025-06-06 17:35:27 -0500

    Update index.tsx

--- decanaturas/src/services/components/index.tsx
+++ decanaturas/src/services/components/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from 'App.js';
 import Decanaturas from 'pages/PagesDecanaturas/components/index';
-import DecanaturaIEST from 'pages/PagesDecanaturas/PagesIngEstadistica/components/index';
 import IESTAct1 from 'pages/PagesDecanaturas/PagesIngEstadistica/Activities/components/ActEst1/index';
 import IAAct1 from 'pages/PagesDecanaturas/PagesIngIA/Activities/components/ActIA1/index';
 import NotFound from 'core/components/NotFound/components/notFound';
-import ISISAct1 from 'pages/PagesDecanaturas/PagesIngSistemas/Activities/components/index';
 import CiberAct1 from 'pages/PagesDecanaturas/PagesIngCiberSeguridad/Activities/components/ActCiber1/index';
 
 const basename = process.env.PUBLIC_URL || '/';
